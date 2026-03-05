4d000f84a7509fae27da1b58ed59b2fab200a1ea
rethapp-member1 <rethapp@protonmail.com>
2026-03-05 01:25:54 +0100

    Update skill 14

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,25 @@
-use std::collections::VecDeque;
-use std::io::Read;
-use std::net::TcpStream;
-use std::str::from_utf8;
+use reqwest;
+use serde_json::Value;
 
-fn main() {
-    let mut stream = TcpStream::connect("repubblica.it").expect("Failed to connect to Repubblica.it");
-    let mut buffer = VecDeque::new();
+#[derive(serde::Deserialize)]
+struct NewsItem {
+    title: String,
+    link: String,
+    pub_date: String,
+}
 
-    stream.read_to_end(&mut buffer).expect("Failed to read from Repubblica.it");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://repubblica.it/api/v2/news";
+    let response = reqwest::get(url).await?;
+    let news: Vec<NewsItem> = response.json().await?;
 
-    let html = String::from_utf8(buffer).expect("Failed to convert buffer to string");
-
-    // Simple HTML parsing to find the main news section
-    let start_index = html.find("<div class=\"news-list\">").expect("Failed to find news list");
-    let end_index = html.find("</div>").expect("Failed to find closing news list tag");
-
-    let news_section = &html[start_index..end_index];
-
-    let mut news = Vec::new();
-    let news_items = news_section.split("<article>");
-
-    for item in news_items {
-        if item.contains("<h3>") {
-            let title_start = item.find("<h3>").expect("Failed to find news title");
-            let title_end = item.find("</h3>").expect("Failed to find closing news title tag");
-            let title = &item[title_start..title_end];
-            news.push(title);
-        }
+    println!("Latest News:");
+    for (i, news_item) in news.iter().enumerate().take(3) {
+        println!("#{} {} - {}", i + 1, news_item.title, news_item.pub_date);
+        println!("Link: {}", news_item.link);
+        println!();
     }
 
-    // Limit to 3 news
-    let news = news.into_iter().take(3).collect();
-
-    for news in news {
-        println!("{}", news);
-    }
+    Ok(())
 }
