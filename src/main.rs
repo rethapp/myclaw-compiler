0ce2ebadc3c374bd8c788a287a327b68c8927b9f
rethapp-member1 <rethapp@protonmail.com>
2026-03-05 01:28:48 +0100

    Update skill 17

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,28 @@
-use std::fs;
-use std::net::TcpStream;
-use std::io::{self, Read, Write};
-use std::time::Duration;
+use reqwest;
+use serde_json::Value;
+use std::collections::HashMap;
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let mut stream = TcpStream::connect("repubblica.it:80").await?;
-    stream.set_read_timeout(Some(Duration::from_secs(5)))?;
-    stream.set_write_timeout(Some(Duration::from_secs(5)))?;
-
-    let request = format!(
-        "GET / HTTP/1.1\r\nHost: repubblica.it\r\nConnection: close\r\n\r\n"
-    );
-
-    stream.write_all(request.as_bytes()).await?;
-
-    let mut response = String::new();
-    stream.read_to_string(&mut response).await?;
+fn main() {
+    let url = "https://www.repubblica.it/rss/ultime-notizie.xml";
+    let response = reqwest::get(url).unwrap();
+    let xml_content = response.text().unwrap();
 
     let mut news = Vec::new();
-    let mut lines = response.lines();
+    let mut parser = xmltree::XMLTree::parse(&xml_content).unwrap();
+    let channel = parser.descendants().find(|node| node.tag.name == "channel").unwrap();
 
-    while let Some(line) = lines.next() {
-        if line.starts_with("<title>") {
-            let title = line.replace("<title>", "").replace("</title>", "");
-            news.push(title);
-        }
-        if news.len() == 3 {
-            break;
-        }
+    for item in channel.descendants().filter(|node| node.tag.name == "item") {
+        let title = item.descendants().find(|node| node.tag.name == "title").unwrap().text().unwrap();
+        let link = item.descendants().find(|node| node.tag.name == "link").unwrap().text().unwrap();
+        news.push((title, link));
     }
 
-    for (i, news) in news.iter().enumerate() {
-        println!("{}. {}", i + 1, news);
-    }
+    let mut top_news: Vec<_> = news.into_iter().take(3).collect();
+    top_news.sort_by(|a, b| a.0.cmp(&b.0));
 
-    Ok(())
+    for (title, link) in top_news {
+        println!("Title: {}", title);
+        println!("Link: {}", link);
+        println!();
+    }
 }
