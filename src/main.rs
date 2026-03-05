12d4370d774f5f921bcbe6a708be1e72b7290dc7
rethapp-member1 <rethapp@protonmail.com>
2026-03-05 01:23:04 +0100

    Update skill 11

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,31 @@
-use reqwest::get;
+use reqwest;
 use serde_json::Value;
 
-fn main() {
-    let url = "https://www.repubblica.it/rss/news/rss.xml";
-    let response = get(url).expect("Failed to fetch the news feed");
-    let xml_content = response.text().expect("Failed to read the XML content");
+fn get_latest_news() -> Result<Vec<String>, reqwest::Error> {
+    let url = "https://repubblica.it/rss/ultimi-news/";
+    let response = reqwest::get(url)?.text()?;
+    let json: Value = serde_json::from_str(&response)?;
 
-    let news: Vec<Value> = serde_xml_rs::from_str(&xml_content).expect("Failed to parse the XML into JSON");
-    let mut top_news = Vec::new();
-
-    for news_item in &news {
-        if let Some(title) = news_item.get("title") {
-            top_news.push(title.as_str().unwrap().to_string());
-            if top_news.len() == 3 {
-                break;
-            }
+    let mut news = Vec::new();
+    for item in json["rss"]["channel"]["item"].as_array().unwrap() {
+        news.push(item["title"].as_str().unwrap().to_string());
+        if news.len() == 3 {
+            break;
         }
     }
 
-    println!("Top 3 news articles:");
-    for (index, article) in top_news.iter().enumerate() {
-        println!("{}. {}", index + 1, article);
+    Ok(news)
+}
+
+fn main() {
+    match get_latest_news() {
+        Ok(news) => {
+            for news in news {
+                println!("{}", news);
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to fetch news: {}", e);
+        }
     }
 }
